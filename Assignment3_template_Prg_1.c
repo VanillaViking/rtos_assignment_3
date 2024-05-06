44142d21e111a6788156be65c352f0f2168ad130
Ashwin Rajesh <ashwinr2k2@gmail.com>
2024-05-06 22:27:29 +1000

    working part1

--- Assignment3_template_Prg_1.c
+++ Assignment3_template_Prg_1.c
@@ -28,15 +28,17 @@ typedef struct {
    float arrive_t, burst_t;
    float wait_t;
    float turn_around_t;
+   float last_active_t;
+   int arrived;
 } process;
 
 typedef struct RR_Params {
    //add your variables here
-   int pipeFile[2]; // [0] for read and [1] for write. use pipe for data transfer from thread A to thread B
+   char* fifo_filename;
    process *process_list;
    int num_processes;
    int timequant;
-
+   char* output_filename;
 } ThreadParams;
 
 pthread_attr_t attr;
@@ -44,7 +46,8 @@ pthread_attr_t attr;
 //TODO: revert optimisation level in makefile
 
 void initializeData(ThreadParams *params, int argc, char** argv);
-double rr_serve(process* proc_list, int list_len, int process_idx, int quant);
+double rr_serve(process* proc_list, int process_idx, int quant, double current_time);
+void sort(process p[], int num);
 
 
 /* this function calculates Round Robin (RR) with a time quantum of 4, writes waiting time and turn-around time to the RR */
@@ -62,32 +65,74 @@ void *worker1(void *params)
 
       if (!is_empty(ready_queue)) {
 	 selected_process = dequeue(ready_queue);
-	 // allow the selected process to run, while putting other ready processes on wait
-	 duration = rr_serve(tp->process_list, tp->num_processes, selected_process, tp->timequant);
+	 duration = rr_serve(tp->process_list, selected_process, tp->timequant, current_time);
       }
       current_time += duration;
 
       // check if any more processes are ready
       for (int n = 0; n < tp->num_processes; n++) {
 	 process proc = tp->process_list[n];
-
-	 if (proc.arrive_t <= current_time) {
-	    //process has arrived
+	 if (!proc.arrived && proc.arrive_t <= current_time) {
+	    tp->process_list[n].arrived = 1;
 	    enqueue(ready_queue, n);
 	 }
       }
-      if (selected_process >= 0 && tp->process_list[selected_process].burst_t > 0) {
-	 enqueue(ready_queue, selected_process);
-      } else if (selected_process >= 0) {
-	 // the process has finished executing
-	 finished_processes++;
+      
+      // put currently selected process back in queue if it is yet to finish
+      if (selected_process >= 0) {
+	 if (tp->process_list[selected_process].burst_t > 0) {
+	    enqueue(ready_queue, selected_process);
+	 } else {
+	    finished_processes++;
+	    tp->process_list[selected_process].turn_around_t = current_time - tp->process_list[selected_process].arrive_t;
+	 }
       }
    }
+
+   float avg_wait;
+   float avg_turnaround;
+   float total_wait = 0;
+   float total_turnaround = 0;
+
+   for (int i = 0; i < tp->num_processes; i++) {
+      /* printf("[%d] wait: %.2f, turnaround: %.2f\n", tp->process_list[i].pid, tp->process_list[i].wait_t, tp->process_list[i].turn_around_t); */
+      total_wait += tp->process_list[i].wait_t;
+      total_turnaround += tp->process_list[i].turn_around_t;
+   }
+
+   avg_wait = total_wait / tp->num_processes;
+   avg_turnaround = total_turnaround / tp->num_processes;
+
+   /* printf("[AVG] wait: %.2f, turnaround: %.2f\n", avg_wait, avg_turnaround); */
+   int fifo_fd = open(tp->fifo_filename, O_WRONLY);
+
+   char message[80];
+   sprintf(message, "%.2f, %.2f", avg_wait, avg_turnaround);
+   write(fifo_fd, message, strlen(message)+1);
+   close(fifo_fd);
+
 }
 
 /* reads the waiting time and turn-around time through the RR and writes to text file */
-void *worker2()
+void *worker2(void* params)
 {
+   ThreadParams *tp = (ThreadParams*) params;
+
+   int fifo_fd = open(tp->fifo_filename, O_RDONLY);
+
+   char message[80];
+   read(fifo_fd, message, sizeof(message));
+
+   char* delim = ",";
+
+   float wait_t = atof(strtok(message, delim));
+   float turnaround_t = atof(strtok(NULL, delim));
+
+   FILE* output_fp = fopen(tp->output_filename, "w");
+
+   printf("Wait Time: %.2f\nTurnaround Time: %.2f\n", wait_t, turnaround_t);
+   printf("written to file %s\n", tp->output_filename);
+   fprintf(output_fp, "Wait Time: %.2f\nTurnaround Time: %.2f\n", wait_t, turnaround_t);
 }
 
 /* this main function creates named pipe and threads */
@@ -119,44 +164,46 @@ int main(int argc, char** argv)
 	 return 0;
 }
 
-double rr_serve(process* proc_list, int list_len, int process_idx, int quant) {
+// allow the selected process to run, & update it's waiting time
+double rr_serve(process* proc_list, int process_idx, int quant, double current_time) {
    double duration = quant + 0.0;
    if (proc_list[process_idx].burst_t < quant) {
       duration = proc_list[process_idx].burst_t; 
    }
 
-   for (int n = 0; n < list_len; n++) {
-      if (n == process_idx) {
-	 proc_list[n].burst_t -= duration;
-      } else {
-	 proc_list[n].wait_t += duration;
-      }
-   }
+   proc_list[process_idx].burst_t -= duration;
+   proc_list[process_idx].wait_t += current_time - proc_list[process_idx].last_active_t;
+   proc_list[process_idx].last_active_t = current_time + duration;
 
-   return duration
+   return duration;
 }
 
 
 void initializeData(ThreadParams *params, int argc, char** argv) {
 
   /* Verify the correct number of arguments were passed in */
-  if (argc != 2) {
+  if (argc != 3) {
     fprintf(stderr, "USAGE:./assign2 timequant output.txt\n");
     exit(1);
   }
 
   params->timequant = atoi(argv[1]);
+  params->output_filename = argv[2];
 
   if (params->timequant <= 0) {
     fprintf(stderr, "invalid time quantum\n");
     exit(1);
   }
+   
+   // remove file from previous sessions to ensure that fifo creation does not fail
+   remove("/tmp/task3fifo");
 
-   // initialize pipe
-   int result = pipe(params->pipeFile);
-   if (result < 0){
-      perror("pipe error\n");
-      exit(1);
+   // initialize fifo
+   params->fifo_filename = "/tmp/task3fifo";
+   int fifo_result = mkfifo(params->fifo_filename, 0666);
+   if (fifo_result != 0) {
+    fprintf(stderr, "Failed to initialise named pipe.\n");
+    exit(1);
    }
 
    params->num_processes = 7;
@@ -165,29 +212,65 @@ void initializeData(ThreadParams *params, int argc, char** argv) {
    params->process_list[0].pid=1;
    params->process_list[0].arrive_t=8;
    params->process_list[0].burst_t=10;
+   params->process_list[0].arrived=0;
+   params->process_list[0].last_active_t=params->process_list[0].arrive_t;
 
    params->process_list[1].pid=2;
    params->process_list[1].arrive_t=10;
    params->process_list[1].burst_t=3;
+   params->process_list[1].arrived=0;
+   params->process_list[1].last_active_t=params->process_list[1].arrive_t;
 
    params->process_list[2].pid=3;
    params->process_list[2].arrive_t=14;
    params->process_list[2].burst_t=7;
+   params->process_list[2].arrived=0;
+   params->process_list[2].last_active_t=params->process_list[2].arrive_t;
 
    params->process_list[3].pid=4;
    params->process_list[3].arrive_t=9;
    params->process_list[3].burst_t=5;
+   params->process_list[3].arrived=0;
+   params->process_list[3].last_active_t=params->process_list[3].arrive_t;
    
    params->process_list[4].pid=5;
    params->process_list[4].arrive_t=16;
    params->process_list[4].burst_t=4;
+   params->process_list[4].arrived=0;
+   params->process_list[4].last_active_t=params->process_list[4].arrive_t;
   
    params->process_list[5].pid=6;
    params->process_list[5].arrive_t=21;
    params->process_list[5].burst_t=6;
+   params->process_list[5].arrived=0;
+   params->process_list[5].last_active_t=params->process_list[5].arrive_t;
  
    params->process_list[6].pid=7;
    params->process_list[6].arrive_t=26;
    params->process_list[6].burst_t=2;
+   params->process_list[6].arrived=0;
+   params->process_list[6].last_active_t=params->process_list[6].arrive_t;
 
+   //sort by arrive time to simulate real world process scheduling
+   sort(params->process_list, params->num_processes);
+
+}
+
+void sort(process p[], int num)
+{
+   int i,j;
+   process temp;
+   for (i = 0; i<num; i++)
+   {
+        for (j = i+1; j<num; j++)
+        {
+ 
+	if(p[i].arrive_t > p[j].arrive_t) 	
+           {
+                temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
+           }
+        }
+   }
 }
